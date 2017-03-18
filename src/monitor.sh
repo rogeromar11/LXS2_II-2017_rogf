c7926757429c86a66694be8bad70d969457cd179
Roger Omar G <rogeromar11@gmail.com>
2017-03-18 14:46:25 -0600

    Generalizacion del scrip para reportes

--- src/monitor.sh
+++ src/monitor.sh
@@ -3,15 +3,22 @@
 #Programa para realizar respaldos de documentacion
 
 #Ambos comandos son equivalentes
-current_pwd=`pwd`
-current_pwd=$(pwd)
+#current_pwd=`pwd`
+#current_pwd=$(pwd)
+
+#read -p "Ingrese ruta a respaldar: " dir
+
+echo "Ingrese ruta a respaldar: "
+read dir
 
 cd /usr/share/doc
 echo Reporte: >> /tmp/reporte
 date >> /tmp/reporte
-pwd >> /tmp/reporte
-du -sh . >> /tmp/reporte
+echo $dir >> /tmp/reporte
+du -sh $dir >> /tmp/reporte
 
 echo PAth de ejecucion `pwd`
 
-cd $current_pwd
+#cd $current_pwd
+
+exit 0
