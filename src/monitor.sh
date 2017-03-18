3dde6aa847cbb2ca38cc9c333435fdeae5399e96
Roger Omar G <rogeromar11@gmail.com>
2017-03-18 13:58:01 -0600

    Actualizacion del archivo

--- src/monitor.sh
+++ src/monitor.sh
@@ -1,6 +1,17 @@
 #!bin/bash
+
+#Programa para realizar respaldos de documentacion
+
+#Ambos comandos son equivalentes
+current_pwd=`pwd`
+current_pwd=$(pwd)
+
 cd /usr/share/doc
 echo Reporte: >> /tmp/reporte
 date >> /tmp/reporte
 pwd >> /tmp/reporte
 du -sh . >> /tmp/reporte
+
+echo PAth de ejecucion `pwd`
+
+cd $current_pwd
