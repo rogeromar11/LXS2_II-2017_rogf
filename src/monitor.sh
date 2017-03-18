3f80dc720eae7fdbf40df085f58112f240877569
Roger Omar G <rogeromar11@gmail.com>
2017-03-18 15:03:28 -0600

    Actualizacion de datos

--- src/monitor.sh
+++ src/monitor.sh
@@ -6,18 +6,15 @@
 #current_pwd=`pwd`
 #current_pwd=$(pwd)
 
-#read -p "Ingrese ruta a respaldar: " dir
+read -p "Ingrese ruta a respaldar: " dir
 
-echo "Ingrese ruta a respaldar: "
-read dir
+#echo "Ingrese ruta a respaldar: "
+#read dir
 
-cd /usr/share/doc
 echo Reporte: >> /tmp/reporte
 date >> /tmp/reporte
 echo $dir >> /tmp/reporte
 du -sh $dir >> /tmp/reporte
-
-echo PAth de ejecucion `pwd`
 
 #cd $current_pwd
 
